ee067a2491a4d93fb8cc7c4f31e027904f33a4d2
Martin Strohm <martin.strohm@helsinki.fi>
2019-05-14 15:18:24 +0300

    Added user component

---
+++ massage-booking-system-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const App = () => {
+return (
+   <div>
+       <button>Book a Massage</button> <button>My Appointments</button>
+   </div> 
+   
+)
+}
+
+export default App
