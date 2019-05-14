514aff82891ac39d2d6b5c0f1f4ae6f7d9fff0d2
Martin Strohm <martin.strohm@helsinki.fi>
2019-05-14 16:04:22 +0300

    appointment listing

--- massage-booking-system-frontend/src/App.js
+++ massage-booking-system-frontend/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import appointmentService from './services/appointments'
+import axios from 'axios'
 const App = (props) => {
+    const [appointments, setAppointments] = useState([])
+
+useEffect(()=>{
+    appointmentService
+    .getAppointments()
+}
+)
+
+    const listAppointments = () =>{
+        appointments.map(u =>
+            <p>{u.id}</p>)
+    }
+
+
 return (
+    <div>
+    <div>
+        {listAppointments}
+     </div>   
    <div>
-       <button>Book a Massage</button> <button>My Appointments</button>
+       <button>Book a Massage</button> 
+       <button>My Appointments</button>
+   </div>
    </div> 
    
 )
