f4575a1e5516654fc661d4e81c8253b456068c4e
koivkai <kaius.koivunen@gmail.com>
2019-05-15 14:05:48 +0300

    merge pull

--- massage-booking-system-frontend/src/App.js
+++ massage-booking-system-frontend/src/App.js
@@ -4,6 +4,8 @@ import masseussesService from "./services/masseusses"
 import appointmentsService from "./services/appointments"
 import calenderService from "./services/calender"
 import Calender from "./components/Calender"
+import Toggleable from "./components/Toggleable";
+import { timingSafeEqual } from "crypto";
 
 
 const App = () => {
@@ -11,25 +13,37 @@ const App = () => {
   const [masseusses, setMasseusses] = useState([])
   const [appointments, setAppointments] = useState([])
   const [times, setTimes] = useState([])
+  const [weeks, setWeeks] = useState([])
+  
 
 
   useEffect(() => {
+    console.log('useEfect')
     usersService.getUsers().then((response => setUsers(response.data)))
     appointmentsService.getAppointments().then((response => setAppointments(response.data)))
     masseussesService.getMasseusses().then((response => setMasseusses(response.data)))
     calenderService.getTimes().then((response => setTimes(response.data)))
+    
 
   }, [])
-
-  console.log(users)
+  let intialDay = times[0]
+  console.log('initialday', intialDay)
+  const [day, setDay] = useState([])
+  
+  
+  console.log('times', times)
+  
+ /*  console.log(users)
   console.log(masseusses)
   console.log(appointments)
-  console.log(times)
+  console.log(times) */
 
   return (
     <div>
+      
       <p>Hello world</p>
-      <Calender times={times} />
+      <Toggleable day={day} setDays={setDay} />
+      <Calender times={day} />
     </div>
   )
 }
---
+++ massage-booking-system-frontend/src/components/Toggleable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Toggleable = (props) => {
+
+    
+
+    return (
+        <div></div>
+    )
+    
+    }
+export default Toggleable
