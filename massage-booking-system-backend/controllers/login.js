1174ca56852d6faf4875c826b9f38ce9990e3ff2
Anton <anton.moroz@helsinki.fi>
2019-05-15 15:56:03 +0300

    Fixed login problems. Can now use POST endpoint for logging in existing users

--- massage-booking-system-backend/controllers/login.js
+++ massage-booking-system-backend/controllers/login.js
@@ -1,11 +1,14 @@
 const jsonWebToken = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
+const express = require('express')
+const bodyParser = require('body-parser')
+const loginRouter = express.Router()
+loginRouter.use(bodyParser.json())
 const User = require('../models/user')
 
 loginRouter.post('/', async (req, res) => {
   const body = req.body
-  console.log('request body', req.body)
+  console.log('request body', body)
 
   /*
   Data from email and password fields
@@ -31,7 +34,7 @@ loginRouter.post('/', async (req, res) => {
   }
 
   const token = jsonWebToken.sign(userCheckForTokenObject, process.env.SECRET)
-  response
+  res
     .status(200)
     .send({ token, email: foundUser.email, name: foundUser.name })
 
