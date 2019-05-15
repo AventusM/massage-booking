c8885125ac091033bfefceba3ef6e85734969d8e
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-05-15 11:03:12 +0000

    Implement mongoose and connecting to MongoDB

--- massage-booking-system-backend/app.js
+++ massage-booking-system-backend/app.js
@@ -2,11 +2,21 @@ const http = require('http')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const mongoose = require('mongoose')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+})
 
 const masseussesRouter = require('./controllers/masseusses')
 const usersRouter = require('./controllers/users')
@@ -20,6 +30,5 @@ app.use(middleware.errorHandler)
 app.use(cors())
 app.use(express.static('build'))
 
-const server = http.createServer(app)
 
 module.exports = app
