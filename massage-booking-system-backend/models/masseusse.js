1cbdb9719178429e0521ed7193cfb1b1fe5752c3
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-05-15 11:36:27 +0000

    Update users controller and user model for mongoose

--- massage-booking-system-backend/models/masseusse.js
+++ massage-booking-system-backend/models/masseusse.js
@@ -12,18 +12,5 @@ const masseusseSchema = mongoose.Schema({
 })
 
 const Masseusse = mongoose.model('Masseusse', masseusseSchema)
+
 module.exports = Masseusse
-
-
-// let masseusses = [
-//   {
-//     "id": 1,
-//     "name": "Hanna Hieroja"
-//   },
-//   {
-//     "id": 2,
-//     "name": "Manne Massager"
-//   }
-// ]
-
-// module.exports = masseusses
