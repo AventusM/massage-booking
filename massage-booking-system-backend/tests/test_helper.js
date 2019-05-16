50a25cefa3d495916005d877b2440cec3d169207
Anton <anton.moroz@helsinki.fi>
2019-05-16 14:19:27 +0300

    Add few tests for USER - GET endpoint. Remove unnecessary beginning class

--- massage-booking-system-backend/tests/test_helper.js
+++ massage-booking-system-backend/tests/test_helper.js
@@ -7,8 +7,22 @@ const initialHelperUsers = [
     email: "keijo@kayttaja.fi",
     admin: false,
     passwordHash: "hdkjashdkjsadh"
+  },
+  {
+    name: "Kalle Keittäjä",
+    number: "050-1231231",
+    email: "kalle@keittäjä.fi",
+    admin: false,
+    passwordHash: "jsadjhsa"
   }
 ]
+
+const fakeId = async () => {
+  const fakeUser = new User(initialHelperUsers[1])
+  await fakeUser.save()
+  await fakeUser.remove()
+  return fakeUser._id.toString()
+}
 
 const usersFromDb = async () => {
   const users = await User.find({})
@@ -16,5 +30,5 @@ const usersFromDb = async () => {
 }
 
 module.exports = {
-  usersFromDb, initialHelperUsers
+  usersFromDb, initialHelperUsers, fakeId
 }
--- massage-booking-system-backend/tests/users.test.js
+++ massage-booking-system-backend/tests/users.test.js
@@ -5,7 +5,7 @@ const app = require('../app')
 const api = supertest(app)
 const User = require('../models/user')
 
-describe('with an existing USER', () => {
+describe('with existing USERS', () => {
 
   beforeEach(async () => {
     await User.deleteMany({})
@@ -14,9 +14,45 @@ describe('with an existing USER', () => {
     await Promise.all(promiseArray)
   })
 
-  it('returns initially a SINGLE USER', async () => {
+  it('displays correct user amount', async () => {
     const response = await api.get('/api/users')
-    console.log('res body', response.body)
+    expect(response.body.length).toBe(helper.initialHelperUsers.length)
+  })
+
+  it('displays correct data from FETCHED DATA to INITIAL DATA', async () => {
+    const response = await api.get('/api/users')
+    const body = response.body
+    
+    const names = body.map(item => item.name)
+    const emails = body.map(item => item.email)
+    const numbers = body.map(item => item.number)
+
+    expect(names).toContain(helper.initialHelperUsers[0].name)
+    expect(names).toContain(helper.initialHelperUsers[1].name)
+
+    expect(emails).toContain(helper.initialHelperUsers[0].email)
+    expect(emails).toContain(helper.initialHelperUsers[1].email)
+
+    expect(numbers).toContain(helper.initialHelperUsers[0].number)
+    expect(numbers).toContain(helper.initialHelperUsers[1].number)
+
+  })
+
+  it('returns 500 status code if a non-existing user is fetched', async () => {
+    const removedId = await helper.fakeId()
+    // console.log('removed id', removedId)
+    const response = await api.get(`/api/users/${removedId}`)
+    expect(response.status).toBe(500)
+  })
+
+  it('fails to fetch a user with INVALID id', async () => {
+    let badIdvalue = "5a3d5da59070081a82a3445"
+    const response = await api
+      .get(`/api/users/${badIdvalue}`)
+      .expect(400)
+
+    const message = response.body.error
+    expect(message).toEqual('malformatted id')
   })
 
 })
