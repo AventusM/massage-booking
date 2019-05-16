89677a88aae9e6d5bc270f6232dc39c3ff49336e
Anton <anton.moroz@helsinki.fi>
2019-05-16 12:51:45 +0300

    Adding a helper user for actual testing

--- massage-booking-system-backend/tests/test_helper.js
+++ massage-booking-system-backend/tests/test_helper.js
@@ -1,10 +1,18 @@
 const User = require('../models/user')
 
+const initialHelperUser = {
+  name: "Keijo Käyttäjä",
+  number: "050-1231231",
+  email: "keijo@kayttaja.fi",
+  admin: false,
+  password: "test"
+}
+
 const usersFromDb = async () => {
   const users = await User.find({})
   return users.map(user => user.toJSON())
 }
 
 module.exports = {
-  usersFromDb
+  usersFromDb, initialHelperUser
 }
