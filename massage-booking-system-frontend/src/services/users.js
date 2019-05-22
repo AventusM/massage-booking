df82879b9afa04f5340fbd38d4d2252cc8d60990
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-05-22 10:37:42 +0300

    Extend users service to handle creating and updating users

--- massage-booking-system-frontend/src/services/users.js
+++ massage-booking-system-frontend/src/services/users.js
@@ -5,4 +5,14 @@ const getUsers = () => {
   return axios.get(baseUrl)
 }
 
+const addUser = async newObject => {
+  const response = await axios.post(baseUrl, newObject)
+  return response.data
+}
+
+const updateUser = (id, newObject) => {
+  const request = axios.put(`${baseUrl} /${id}`, newObject)
+  return request.then(response => response.data)
+}
+
 export default { getUsers }
