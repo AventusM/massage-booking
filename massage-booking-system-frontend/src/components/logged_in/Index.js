eec8da327da3fb7210174c5da131f8f467236e57
Anton <anton.moroz@helsinki.fi>
2019-05-30 13:19:48 +0300

    More responsive changes to Index.js

--- massage-booking-system-frontend/src/components/logged_in/Index.js
+++ massage-booking-system-frontend/src/components/logged_in/Index.js
@@ -19,7 +19,6 @@ const Index = (props) => {
       <div className="appointmentListWrapperMain">
         <div className="appointmentListWrapperCalendar">
           <Calendar
-            showWeekNumbers={true}
             locale={"en-UK"}
             onChange={(value) => {
               console.log('value ', value, 'value type', typeof value)
