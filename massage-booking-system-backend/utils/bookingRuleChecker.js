e8ce8311a872229e330b77070950b8874cfd064e
koivkai <kaius.koivunen@gmail.com>
2019-06-04 12:52:38 +0300

    wip commit, so that I can change branches handle pull request

--- massage-booking-system-backend/utils/bookingRuleChecker.js
+++ massage-booking-system-backend/utils/bookingRuleChecker.js
@@ -7,10 +7,22 @@ const userAllowedToMakeAppointment = async (usersAppointmentList, requestedAppoi
         const appointment = await Appointment.findById(requestedAppointmentID)
         let usersPreviousMassageTimes = usersAppointmentList.map(appointment => appointment.start_date)
         console.log('users appointment history ', usersPreviousMassageTimes)
+
+        let now = moment()
         let appointmentTimeMoment = moment(appointment.start_date)
         console.log('appointmentTimeMoment', appointmentTimeMoment)
+
+        if(appointmentTimeMoment.isBefore(now)) { // cant book past times
+            console.log('Tried to book past date')
+            return false
+        }
+
+        let appointmentWeekOfYear =  appointmentTimeMoment.week
+        let appointmentsFirstDayOfTheWeek = appointmentTimeMoment.startOf('week')
+        console.log('first day of the week appoint is being booked in', appointmentsFirstDayOfTheWeek)
         usersPreviousMassageTimes = usersPreviousMassageTimes.filter((prevTime) => {
             let prevTimeMoment = moment(prevTime)
+
             let dayDifference = appointmentTimeMoment.diff(prevTimeMoment, 'days')
             console.log('day difference', dayDifference)
             return Math.abs(dayDifference) < 14
