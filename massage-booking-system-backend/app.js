9f3172aa07f3687f147c3c4d01986cb8d02f8276
Anton <anton.moroz@helsinki.fi>
2019-06-04 15:10:54 +0300

    Working with getting google sign in to work

--- massage-booking-system-backend/app.js
+++ massage-booking-system-backend/app.js
@@ -6,13 +6,32 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const router = express.Router()
 const jsonWebToken = require('jsonwebtoken')
-const passport = require('passport');
-
 
 // const protectedRoute = require('./utils/protectedRoute')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+passport.use(new GoogleStrategy({
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+  callbackURL: '/auth/google/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    console.log('accesstoken', accessToken)
+    console.log('refreshtoken', refreshToken)
+    console.log('profile', profile)
+  }))
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 logger.info('connecting to', config.MONGODB_URI)
 app.use(cors())
@@ -25,8 +44,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
     logger.error('error connection to MongoDB:', error.message)
   })
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
 const masseussesRouter = require('./controllers/masseusses')
@@ -34,10 +53,7 @@ const appointmentsRouter = require('./controllers/appointments')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const statsRouter = require('./controllers/stats')
-const authRouter = require('./controllers/auth')
 
-
-app.use('/auth', authRouter)
 app.use('/api', router)
 app.use('/api/masseusses', masseussesRouter)
 app.use('/api/appointments', appointmentsRouter)
--- massage-booking-system-backend/utils/config.js
+++ massage-booking-system-backend/utils/config.js
@@ -1,17 +1,21 @@
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-  
-  let PORT = process.env.PORT
-  let MONGODB_URI = process.env.MONGODB_URI
-  
-  
-  if (process.env.NODE_ENV === 'test') {
-    MONGODB_URI = process.env.TEST_MONGODB_URI
-  }
-  
-  module.exports = {
-    MONGODB_URI,
-    PORT
-  }
+  require('dotenv').config()
+}
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+let CLIENT_ID = process.env.CLIENT_ID
+let CLIENT_SECRET = process.env.CLIENT_SECRET
+
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
+module.exports = {
+  MONGODB_URI,
+  PORT,
+  CLIENT_ID,
+  CLIENT_SECRET
+}
