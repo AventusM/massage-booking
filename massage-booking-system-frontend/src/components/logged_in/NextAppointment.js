6d3f3215b5f08cf52668511f6b5ef57fe2adeadb
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-06-12 16:10:54 +0300

    Start work on the SimpleAppointment component

--- massage-booking-system-frontend/src/components/logged_in/NextAppointment.js
+++ massage-booking-system-frontend/src/components/logged_in/NextAppointment.js
@@ -1,9 +1,40 @@
 import React, { useContext, useState } from 'react'
 import Appointment from './Appointment'
 import { AppointmentContext, UserContext } from '../../App'
+const moment = require('moment')
 
 const SimpleAppointment = ({ app }) => {
-  return <p>{app.start_date}</p>
+  const date = new Date(app.start_date)
+  const currentWeek = moment().week()
+  console.log('currentWeek: ', currentWeek)
+  console.log('appointmentWekk:', moment(date).week())
+  const weekdays = {
+    1: 'Monday',
+    2: 'Tuesday',
+  }
+
+  const months = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December',
+  }
+  console.log('date: ', date.getDate())
+  return (
+    <p>
+      Your next appointment is on {weekdays[date.getDay()]}
+      {date.getHours()}:{date.getMinutes()} {months[date.getMonth()]}
+      {date.getDate()}
+    </p>
+  )
 }
 
 const NextAppointment = ({ user, appointments }) => {
