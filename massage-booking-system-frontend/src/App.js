19d1fea08b06da6373271718b5596f7384837ddb
Anton <anton.moroz@helsinki.fi>
2019-06-13 11:05:00 +0300

    Notifications attached to appointment actions. Removed a bunch of old console.logs out of the way

--- massage-booking-system-frontend/src/App.js
+++ massage-booking-system-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, Fragment, createContext } from 'react'
-// Temporarily here for data fetching as authentication is completely server-side now
 import axios from 'axios'
 import {
   BrowserRouter as Router,
@@ -22,9 +21,7 @@ import * as types from './types/types'
 import * as icons from './types/fa-icons'
 
 const App = () => {
-  // userService CONTAINS APPOINTMENT ID
   const [users, userService] = useResource('/api/users')
-  // appointmentService FETCHES ALL apps AND also all users apps by ID
   const [appointments, appointmentService] = useResource('/api/appointments')
   const [stats, statsService] = useResource('api/stats')
 
@@ -51,7 +48,7 @@ const App = () => {
       setNotification(null)
       setType(null)
       setIcon(null)
-    }, 5000)
+    }, 3500)
   }
 
   useEffect(() => {
@@ -76,37 +73,32 @@ const App = () => {
       <Router>
         <Header user={user} />
         <Notification icon={notification_icon} type={notification_type} message={notification} />
-        <UserContext.Provider value={{ user, setUser, users, userService }}>
-          <AppointmentContext.Provider
-            value={{ user, appointments, appointmentService, selectedDate, setSelectedDate, setErrorMessage, createNotification }}>
-            <Route exact path="/" render={() => <Index />} />
+
+        <NotificationContext.Provider value={{ createNotification }}>
+          <UserContext.Provider value={{ user, setUser, users, userService }}>
+            <AppointmentContext.Provider
+              value={{ user, appointments, appointmentService, selectedDate, setSelectedDate, setErrorMessage, createNotification }}>
+              <Route exact path="/" render={() => <Index />} />
+            </AppointmentContext.Provider>
+          </UserContext.Provider>
+
+          <UserContext.Provider value={{ user, setUser, users, userService }}>
+            <Route exact path="/dashboard" render={() => <DashBoard />} />
+          </UserContext.Provider>
+
+          <UserContext.Provider value={{ user, setUser, users, userService }}>
+            <AppointmentContext.Provider value={{ user, appointments, appointmentService, selectedDate, setSelectedDate, setErrorMessage }}>
+              <Route exact path="/mypage" render={() => <MyPage />} />
+            </AppointmentContext.Provider>
+          </UserContext.Provider>
+
+          <AppointmentContext.Provider value={{ appointments, appointmentService, stats }}>
+            <Route exact path="/stats" render={() => <Stats />} />
           </AppointmentContext.Provider>
-        </UserContext.Provider>
+        </NotificationContext.Provider>
 
-        <UserContext.Provider value={{ user, setUser, users, userService }}>
-          <Route exact path="/dashboard" render={() => <DashBoard />} />
-        </UserContext.Provider>
-
-        <UserContext.Provider value={{ user, setUser, users, userService }}>
-          <AppointmentContext.Provider
-            value={{
-              user,
-              appointments,
-              appointmentService,
-              selectedDate,
-              setSelectedDate,
-              setErrorMessage,
-            }}
-          >
-            <Route exact path="/mypage" render={() => <MyPage />} />
-          </AppointmentContext.Provider>
-        </UserContext.Provider>
-
-        <AppointmentContext.Provider value={{ appointments, appointmentService, stats }}>
-          <Route exact path="/stats" render={() => <Stats />} />
-        </AppointmentContext.Provider>
       </Router>
-    </Fragment>
+    </Fragment >
   )
 }
 
@@ -129,6 +121,7 @@ const App = () => {
 // CURRENTLY createNotification is being passed around in irrelevant contexts. FIX THIS LATER IN OWN CONTEXT
 // CURRENTLY createNotification is being passed around in irrelevant contexts. FIX THIS LATER IN OWN CONTEXT
 
+export const NotificationContext = createContext(null)
 export const AppointmentContext = createContext(null)
 export const UserContext = createContext(null)
 export default App
--- massage-booking-system-frontend/src/components/logged_in/User.js
+++ massage-booking-system-frontend/src/components/logged_in/User.js
@@ -2,8 +2,6 @@ import React, { useContext } from 'react'
 import { UserContext } from '../../App'
 
 const User = props => {
-  // Order depends on value prop in original provider (this one in App.js)
-  //console.log('UserList.js props', props)
   const { user, userService } = useContext(UserContext)
   const { id, name, email, number, admin, banned, avatarUrl } = props
 
@@ -15,8 +13,8 @@ const User = props => {
         {avatarUrl ? (
           <img src={avatarUrl} alt="profile pic" height="50" width="50" />
         ) : (
-          'avatar'
-        )}
+            'avatar'
+          )}
       </td>
       <td>{name} </td>
       <td>{email}</td>
@@ -40,23 +38,6 @@ const User = props => {
           REMOVE
         </button>
       </td>
-
-      {/*}
-        <div className="dashboard_user_item_actions">
-          <p>Admin actions</p>
-          <button onClick={() => userService.update(id, { admin: !admin, auth_id: user._id })}>
-            {admin
-              ? 'Remove Admin'
-              : 'Create Admin'}
-          </button>
-          <button onClick={() => userService.update(id, { banned: !banned, auth_id: user._id })}>
-            {banned
-              ? 'Unban'
-              : 'Ban'}
-          </button>
-          <button onClick={() => userService.remove(id)}>Remove</button>
-        </div> 
-        */}
     </tr>
   )
 }
