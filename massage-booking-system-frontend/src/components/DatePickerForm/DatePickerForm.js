079ba8caef64de58a5afac8ce70cc7f601866152
Anton <anton.moroz@helsinki.fi>
2019-06-27 12:19:39 +0300

    UX improvement (maybe?) on stretching creation on front-end

--- massage-booking-system-frontend/src/components/DatePickerForm/DatePickerForm.js
+++ massage-booking-system-frontend/src/components/DatePickerForm/DatePickerForm.js
@@ -8,7 +8,7 @@ import getDay from 'date-fns/getDay'
 import Notification from '../Notification/Notification'
 
 const DatePickerForm = () => {
-  const [startDate, setStartDate] = useState(new Date().setHours(8, 55, 0, 0))
+  const [startDate, setStartDate] = useState(undefined)
   const { stretchingService } = useContext(StretchContext)
 
   const handleChange = async (date) => {
@@ -36,8 +36,9 @@ const DatePickerForm = () => {
       <h1>Create a stretching appointment</h1>
       <h2>Select a date below</h2>
 
-      <form className="datepicker_wrapper"onSubmit={createStretch}>
+      <form className="datepicker_wrapper" onSubmit={createStretch}>
         <DatePicker
+          placeholderText="Click to select"
           showTimeSelect
           dateFormat="MMMM d, yyyy HH:mm"
           timeFormat="HH:mm"
@@ -63,7 +64,10 @@ const DatePickerForm = () => {
           onChange={handleChange}
           filterDate={isMondayOrTuesday}
         />
-        <button className = "stretching_submit_button" type="submit">Create</button>
+        {startDate === undefined
+          ? <button disabled className="disabled_submit_button" type="submit">Create</button>
+          : <button className="stretching_submit_button" type="submit">Create</button>
+        }
       </form>
     </div>
   )
